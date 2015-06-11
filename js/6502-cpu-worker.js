6644d8d6f80b5af6bb2b7cd02db4d1f790494987
Felipe Alfonso <manxanaster@gmail.com>
2015-06-11 18:44:10 +0000

    Worker script

---
+++ js/6502-cpu-worker.js
@@ -0,0 +1,25 @@
+var window = {};
+self.importScripts('6502-cpu.js');
+var port;
+self.onconnect = function(e) {
+    port = e.ports[0];
+    port.onmessage = function(e) {
+        if (e.data[0] == 'run') {
+            try {
+                window.CPU6502.runOnThread(port, update);
+            } catch (e) {
+                port.postMessage('Error: ' + e);
+            }
+            port.postMessage('finish');
+            
+        } else if (e.data[0] == 'ram') {
+            window.CPU6502.burn(e.data[1], 0, 65535);
+            port.postMessage(['regdump', window.CPU6502.dumpRegisters()]);
+        }
+    };
+    port.postMessage('connected');
+    port.start();
+};
+var update = function () {
+    port.postMessage(['regdump', window.CPU6502.dumpRegisters()]);
+};
