964895d6071f2de7c1382c869e658109814cb99d
Felipe Alfonso <manxanaster@gmail.com>
2015-06-13 14:32:28 +0000

    Code generation added

--- js/6502-assembler.js
+++ js/6502-assembler.js
@@ -1,12 +1,11 @@
 (function (scope) {
     var ASM6502 = {},
         modeIndex = [
-            'A',
             'IMM',
             'ZP',
             'ZPX',
             'ZPY',
-            'ABS',
+            'AB',
             'ABX',
             'ABY',
             'IND',
@@ -18,63 +17,63 @@
         lookupTable = {
             // Source of lookup table:
             // https://github.com/skilldrick/easy6502/blob/gh-pages/simulator/assembler.js
-            //MN   A    IMM    ZP    ZPX   ZPY   ABS   ABX   ABY  IND   IDX   IDY   IMP   REL
-            ADC: [null, 0x69, 0x65, 0x75, null, 0x6d, 0x7d, 0x79, null, 0x61, 0x71, null, null],
-            AND: [null, 0x29, 0x25, 0x35, null, 0x2d, 0x3d, 0x39, null, 0x21, 0x31, null, null],
-            ASL: [null, null, 0x06, 0x16, null, 0x0e, 0x1e, null, null, null, null, 0x0a, null],
-            BIT: [null, null, 0x24, null, null, 0x2c, null, null, null, null, null, null, null],
-            BPL: [null, null, null, null, null, null, null, null, null, null, null, null, 0x10],
-            BMI: [null, null, null, null, null, null, null, null, null, null, null, null, 0x30],
-            BVC: [null, null, null, null, null, null, null, null, null, null, null, null, 0x50],
-            BVS: [null, null, null, null, null, null, null, null, null, null, null, null, 0x70],
-            BCC: [null, null, null, null, null, null, null, null, null, null, null, null, 0x90],
-            BCS: [null, null, null, null, null, null, null, null, null, null, null, null, 0xb0],
-            BNE: [null, null, null, null, null, null, null, null, null, null, null, null, 0xd0],
-            BEQ: [null, null, null, null, null, null, null, null, null, null, null, null, 0xf0],
-            BRK: [null, null, null, null, null, null, null, null, null, null, null, 0x00, null],
-            CMP: [0xc9, 0xc5, 0xd5, null, 0xcd, 0xdd, 0xd9, null, null, 0xc1, 0xd1, null, null],
-            CPX: [0xe0, 0xe4, null, null, 0xec, null, null, null, null, null, null, null, null],
-            CPY: [0xc0, 0xc4, null, null, 0xcc, null, null, null, null, null, null, null, null],
-            DEC: [null, 0xc6, 0xd6, null, 0xce, 0xde, null, null, null, null, null, null, null],
-            EOR: [0x49, 0x45, 0x55, null, 0x4d, 0x5d, 0x59, null, null, 0x41, 0x51, null, null],
-            CLC: [null, null, null, null, null, null, null, null, null, null, null, 0x18, null],
-            SEC: [null, null, null, null, null, null, null, null, null, null, null, 0x38, null],
-            CLI: [null, null, null, null, null, null, null, null, null, null, null, 0x58, null],
-            SEI: [null, null, null, null, null, null, null, null, null, null, null, 0x78, null],
-            CLV: [null, null, null, null, null, null, null, null, null, null, null, 0xb8, null],
-            CLD: [null, null, null, null, null, null, null, null, null, null, null, 0xd8, null],
-            SED: [null, null, null, null, null, null, null, null, null, null, null, 0xf8, null],
-            INC: [null, 0xe6, 0xf6, null, 0xee, 0xfe, null, null, null, null, null, null, null],
-            JMP: [null, null, null, null, 0x4c, null, null, 0x6c, null, null, null, null, null],
-            JSR: [null, null, null, null, 0x20, null, null, null, null, null, null, null, null],
-            LDA: [0xa9, 0xa5, 0xb5, null, 0xad, 0xbd, 0xb9, null, null, 0xa1, 0xb1, null, null],
-            LDX: [0xa2, 0xa6, null, 0xb6, 0xae, null, 0xbe, null, null, null, null, null, null],
-            LDY: [0xa0, 0xa4, 0xb4, null, 0xac, 0xbc, null, null, null, null, null, null, null],
-            LSR: [null, 0x46, 0x56, null, 0x4e, 0x5e, null, null, null, null, null, 0x4a, null],
-            NOP: [null, null, null, null, null, null, null, null, null, null, null, 0xea, null],
-            ORA: [0x09, 0x05, 0x15, null, 0x0d, 0x1d, 0x19, null, null, 0x01, 0x11, null, null],
-            TAX: [null, null, null, null, null, null, null, null, null, null, null, 0xaa, null],
-            TXA: [null, null, null, null, null, null, null, null, null, null, null, 0x8a, null],
-            DEX: [null, null, null, null, null, null, null, null, null, null, null, 0xca, null],
-            INX: [null, null, null, null, null, null, null, null, null, null, null, 0xe8, null],
-            TAY: [null, null, null, null, null, null, null, null, null, null, null, 0xa8, null],
-            TYA: [null, null, null, null, null, null, null, null, null, null, null, 0x98, null],
-            DEY: [null, null, null, null, null, null, null, null, null, null, null, 0x88, null],
-            INY: [null, null, null, null, null, null, null, null, null, null, null, 0xc8, null],
-            ROR: [null, 0x66, 0x76, null, 0x6e, 0x7e, null, null, null, null, null, 0x6a, null],
-            ROL: [null, 0x26, 0x36, null, 0x2e, 0x3e, null, null, null, null, null, 0x2a, null],
-            RTI: [null, null, null, null, null, null, null, null, null, null, null, 0x40, null],
-            RTS: [null, null, null, null, null, null, null, null, null, null, null, 0x60, null],
-            SBC: [0xe9, 0xe5, 0xf5, null, 0xed, 0xfd, 0xf9, null, null, 0xe1, 0xf1, null, null],
-            STA: [null, 0x85, 0x95, null, 0x8d, 0x9d, 0x99, null, null, 0x81, 0x91, null, null],
-            TXS: [null, null, null, null, null, null, null, null, null, null, null, 0x9a, null],
-            TSX: [null, null, null, null, null, null, null, null, null, null, null, 0xba, null],
-            PHA: [null, null, null, null, null, null, null, null, null, null, null, 0x48, null],
-            PLA: [null, null, null, null, null, null, null, null, null, null, null, 0x68, null],
-            PHP: [null, null, null, null, null, null, null, null, null, null, null, 0x08, null],
-            PLP: [null, null, null, null, null, null, null, null, null, null, null, 0x28, null],
-            STX: [null, 0x86, null, 0x96, 0x8e, null, null, null, null, null, null, null, null],
-            STY: [null, 0x84, 0x94, null, 0x8c, null, null, null, null, null, null, null, null],
+            //MN  IMM    ZP    ZPX   ZPY   ABS   ABX   ABY  IND   IDX   IDY   IMP   REL
+            ADC: [0x69, 0x65, 0x75, null, 0x6d, 0x7d, 0x79, null, 0x61, 0x71, null, null],
+            AND: [0x29, 0x25, 0x35, null, 0x2d, 0x3d, 0x39, null, 0x21, 0x31, null, null],
+            ASL: [null, 0x06, 0x16, null, 0x0e, 0x1e, null, null, null, null, 0x0a, null],
+            BIT: [null, 0x24, null, null, 0x2c, null, null, null, null, null, null, null],
+            BPL: [null, null, null, null, null, null, null, null, null, null, null, 0x10],
+            BMI: [null, null, null, null, null, null, null, null, null, null, null, 0x30],
+            BVC: [null, null, null, null, null, null, null, null, null, null, null, 0x50],
+            BVS: [null, null, null, null, null, null, null, null, null, null, null, 0x70],
+            BCC: [null, null, null, null, null, null, null, null, null, null, null, 0x90],
+            BCS: [null, null, null, null, null, null, null, null, null, null, null, 0xb0],
+            BNE: [null, null, null, null, null, null, null, null, null, null, null, 0xd0],
+            BEQ: [null, null, null, null, null, null, null, null, null, null, null, 0xf0],
+            BRK: [null, null, null, null, null, null, null, null, null, null, 0x00, null],
+            CMP: [0xc9, 0xc5, 0xd5, null, 0xcd, 0xdd, 0xd9, null, 0xc1, 0xd1, null, null],
+            CPX: [0xe0, 0xe4, null, null, 0xec, null, null, null, null, null, null, null],
+            CPY: [0xc0, 0xc4, null, null, 0xcc, null, null, null, null, null, null, null],
+            DEC: [null, 0xc6, 0xd6, null, 0xce, 0xde, null, null, null, null, null, null],
+            EOR: [0x49, 0x45, 0x55, null, 0x4d, 0x5d, 0x59, null, 0x41, 0x51, null, null],
+            CLC: [null, null, null, null, null, null, null, null, null, null, 0x18, null],
+            SEC: [null, null, null, null, null, null, null, null, null, null, 0x38, null],
+            CLI: [null, null, null, null, null, null, null, null, null, null, 0x58, null],
+            SEI: [null, null, null, null, null, null, null, null, null, null, 0x78, null],
+            CLV: [null, null, null, null, null, null, null, null, null, null, 0xb8, null],
+            CLD: [null, null, null, null, null, null, null, null, null, null, 0xd8, null],
+            SED: [null, null, null, null, null, null, null, null, null, null, 0xf8, null],
+            INC: [null, 0xe6, 0xf6, null, 0xee, 0xfe, null, null, null, null, null, null],
+            JMP: [null, null, null, null, 0x4c, null, null, 0x6c, null, null, null, null],
+            JSR: [null, null, null, null, 0x20, null, null, null, null, null, null, null],
+            LDA: [0xa9, 0xa5, 0xb5, null, 0xad, 0xbd, 0xb9, null, 0xa1, 0xb1, null, null],
+            LDX: [0xa2, 0xa6, null, 0xb6, 0xae, null, 0xbe, null, null, null, null, null],
+            LDY: [0xa0, 0xa4, 0xb4, null, 0xac, 0xbc, null, null, null, null, null, null],
+            LSR: [null, 0x46, 0x56, null, 0x4e, 0x5e, null, null, null, null, 0x4a, null],
+            NOP: [null, null, null, null, null, null, null, null, null, null, 0xea, null],
+            ORA: [0x09, 0x05, 0x15, null, 0x0d, 0x1d, 0x19, null, 0x01, 0x11, null, null],
+            TAX: [null, null, null, null, null, null, null, null, null, null, 0xaa, null],
+            TXA: [null, null, null, null, null, null, null, null, null, null, 0x8a, null],
+            DEX: [null, null, null, null, null, null, null, null, null, null, 0xca, null],
+            INX: [null, null, null, null, null, null, null, null, null, null, 0xe8, null],
+            TAY: [null, null, null, null, null, null, null, null, null, null, 0xa8, null],
+            TYA: [null, null, null, null, null, null, null, null, null, null, 0x98, null],
+            DEY: [null, null, null, null, null, null, null, null, null, null, 0x88, null],
+            INY: [null, null, null, null, null, null, null, null, null, null, 0xc8, null],
+            ROR: [null, 0x66, 0x76, null, 0x6e, 0x7e, null, null, null, null, 0x6a, null],
+            ROL: [null, 0x26, 0x36, null, 0x2e, 0x3e, null, null, null, null, 0x2a, null],
+            RTI: [null, null, null, null, null, null, null, null, null, null, 0x40, null],
+            RTS: [null, null, null, null, null, null, null, null, null, null, 0x60, null],
+            SBC: [0xe9, 0xe5, 0xf5, null, 0xed, 0xfd, 0xf9, null, 0xe1, 0xf1, null, null],
+            STA: [null, 0x85, 0x95, null, 0x8d, 0x9d, 0x99, null, 0x81, 0x91, null, null],
+            TXS: [null, null, null, null, null, null, null, null, null, null, 0x9a, null],
+            TSX: [null, null, null, null, null, null, null, null, null, null, 0xba, null],
+            PHA: [null, null, null, null, null, null, null, null, null, null, 0x48, null],
+            PLA: [null, null, null, null, null, null, null, null, null, null, 0x68, null],
+            PHP: [null, null, null, null, null, null, null, null, null, null, 0x08, null],
+            PLP: [null, null, null, null, null, null, null, null, null, null, 0x28, null],
+            STX: [null, 0x86, null, 0x96, 0x8e, null, null, null, null, null, null, null],
+            STY: [null, 0x84, 0x94, null, 0x8c, null, null, null, null, null, null, null]
         },
         mnemonics = [
             'ADC', 'AND', 'ASL', 'BCC',
@@ -513,7 +512,7 @@
                                 var seq = {};
                                 if (token.type == 'mnemonic') {
                                     seq.type = 'op';
-                                    seq.action = token.value;
+                                    seq.opCode = token.value;
                                     ++index;
                                     seq.mode = getAddressingMode();
                                     seq.addr = getAddressValue(seq.mode);
@@ -562,11 +561,107 @@
         }()),
         OCGen = (function () {
             return function (sequence) {
-                  return {
+                var index = 0,
+                    objectCode = [],
+                    lables = {},
+                    eof = function (step) {
+                        return (index + step >= sequence.length);
+                    },
+                    peek = function(step) {
+                        if (!eof(step)) {
+                            return sequence[index + step];
+                        }
+                        return null;
+                    },
+                    readLables = function () {
+                        var idx;
+                        for (idx = 0; idx < sequence.length; ++idx) {
+                            if (sequence[idx].type == 'labeling') {
+                                lables[sequence[idx].labelName] = idx;
+                            }
+                        }
+                    },
+                    resolveByte = function (token) {
+                        if (token.type == 'address_hex' || 
+                            token.type == 'immediate_dec' ||
+                            token.type == 'immediate_hex' ) {
+                            if (token.value > 0xFF) {
+                                var b = [];
+                                b.push((token.value / 256) | 0);
+                                b.push((token.value & 255) | 0);
+                                return b;
+                            }
+                            return token.value;    
+                        } else if (token.type == 'register') {
+                            // null value shouldn't be added to the oc
+                            return null;
+                        } else if (token.type == 'label') {
+                            return 0xFF;
+                        } else {
+                            throw 'Can\'t resolve byte of type ' + token.type;
+                        }
+                    },
+                    lookOpAddress = function (opcode, addrmode) {
+                        var idxmode = modeIndex.indexOf(addrmode);
+                        if (idxmode >= 0) {
+                            if (opcode in lookupTable) {
+                                return lookupTable[opcode][idxmode];
+                            } else {
+                                throw 'Invalid Op Code ' + opcode;
+                            }
+                        } else {
+                            throw 'Invalid address mode '+ addrmode +' for opcode ' + opcode;
+                        }
+                    },
+                    gen = function () {
+                        while (!eof(0) && peek(0) != null) {
+                            var seq = peek(0);
+                            if (seq.type == 'op') {
+                                var addr = lookOpAddress(seq.opCode, seq.mode);
+                                if (addr != null) {
+                                    objectCode.push(addr);
+                                    var args = seq.addr;
+                                    for (var i = 0; i < args.length; ++i) {
+                                        var b = resolveByte(args[i]);
+                                        if (b != null) {
+                                            if (b instanceof Array) {
+                                                objectCode.push(b.pop());
+                                                objectCode.push(b.pop());
+                                            } else {
+                                                objectCode.push(b);
+                                            }
+                                        }
+                                    }
+                                }
+                            } else if (seq.type == 'macrouse') {
+                                for (var i = 0; i < seq.args.length; ++i) {
+                                    var b = resolveByte(seq.args[i]);
+                                    if (b instanceof Array && seq.macroName == 'WORD') {
+                                        objectCode.push(b.pop());
+                                        objectCode.push(b.pop());
+                                    } else if (typeof b == 'number' && seq.macroName == 'BYTE') {
+                                        objectCode.push(b);
+                                    } else {
+                                        throw 'Incorrect byte size for macro ' + seq.macroName;
+                                    }
+                                }
+                            } else if (seq.type != 'labeling') {
+                                throw 'Invalid sequence of type ' + seq.type;
+                            }
+                            ++index;
+                        }
+                    },
+                    dec8ToHex = function (dec) {
+                        var h = dec.toString(16);
+                        return ('00'.substr(0, 2 - h.length) + h).toUpperCase();
+                    };
+                    
+                return {
                     getObjectCode: function () {
                         if (sequence instanceof Array) {
                             if (sequence.length > 0) {
-                                
+                                gen();
+                                return objectCode;
                             } else {
                                 console.log('Empty program.');
                             }
@@ -574,12 +669,38 @@
                             throw 'Invalid sequence stream.';
                         }
                         return null;
-                    }  
-                  };
+                    },
+                    objectCodeDump: function () {
+                        var index,
+                            str = '',
+                            len = objectCode.length;
+                        for (index = 0; index < len; ++index) {
+                            str += dec8ToHex(objectCode[index]) + ' ';
+                            if (index > 0 && index % 16 == 0) {
+                                str += '\n';
+                            }
+                        }
+                        return str;
+                    }
+                };
             };
-        }());
+        }()),
+        lexer,
+        parser,
+        generator;
+        
     ASM6502.processSource = function (source) {
-        return (OCGen(Lexer(source).getTokens()).getSequence()).getObjectCode();
+        var tokens, sequence, objectCode;
+        lexer = Lexer(source);
+        tokens = lexer.getTokens();
+        parser = Parser(tokens);
+        sequence = parser.getSequence(),
+        generator = OCGen(sequence);
+        objectCode = generator.getObjectCode();
+        return objectCode;
+    };
+    ASM6502.dumpObjectCodeToHex = function () {
+        return typeof generator != 'undefined' ? generator.objectCodeDump() : '';
     };
     scope.ASM6502 = ASM6502;
 }(typeof window != 'undefined' ? window : typeof exports != 'undefined' ? exports : {}));
